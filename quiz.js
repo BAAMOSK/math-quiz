160b849d26767b9a37b8b745c06e0846b198df65
BAAMOSK <bunth667@gmail.com>
2017-04-22 22:13:26 -0400

    working on validating questions

--- quiz.js
+++ quiz.js
@@ -1,26 +1,3 @@
-const state = {
-    currentQuestion: 0,
-    quizView: "",
-    choices: ""
-};
-
-let quizView = "<div class=\"status\"><h3 class=\"current\">Question 1 of 5</h3><h3 class=\"score\">Score: 0</h3>"+
-    "</div><h1 class=\"question\">This is the question</h1>" +
-    `<div class="answers">
-      <label><input type="radio" name"option" value="1">Choice 1</label>
-      <label><input type="radio" name"option" value="2">Choice 2</label>
-      <label><input type="radio" name"option" value="3">Choice 3</label>
-      <label><input type="radio" name"option" value="4">Choice 4</label>
-      <label><input type="radio" name"option" value="5">Choice 5</label>
-    </div>` +
-    "<button class=\"submit\">Submit</button>";
-
-$(function() {
-  $(".container").on("click", "button", function() {
-    $(".container").html(quizView);
-  });
-});
-
 let quiz = [
   {
     question: "What is 1 + 3 =",
@@ -38,5 +15,48 @@ let quiz = [
     correct: "30"
   }
 ];
+
+const state = {
+    currentQuestion: 0,
+    questionNumber: 1,
+    questionTotal: quiz.length,
+    score: 0,
+    quizView: "",
+    choices: ""
+};
+
+let answerChoices = {
+    answer1: quiz[state.currentQuestion].choices[0],
+    answer2: quiz[state.currentQuestion].choices[1],
+    answer3: quiz[state.currentQuestion].choices[2],
+    answer4: quiz[state.currentQuestion].choices[3],
+    answer5: quiz[state.currentQuestion].choices[4]
+};
+
+let quizView = "<div class=\"status\"><h3 class=\"current\">"+`Question ${state.questionNumber} of ${state.questionTotal}`+
+"</h3><h3 class=\"score\">"+ `Score: ${state.score}</h3>` + "</div><h1 class=\"question\">"+`${quiz[state.currentQuestion].question}`+"</h1>" +
+    `<div class="answers">
+      <label><input type="radio" name="option" value="${answerChoices.answer1}">${answerChoices.answer1}</label>
+      <label><input type="radio" name="option" value="${answerChoices.answer2}">${answerChoices.answer2}</label>
+      <label><input type="radio" name="option" value="${answerChoices.answer3}">${answerChoices.answer3}</label>
+      <label><input type="radio" name="option" value="${answerChoices.answer4}">${answerChoices.answer4}</label>
+      <label><input type="radio" name="option" value="${answerChoices.answer5}">${answerChoices.answer5}</label>
+    </div>
+    <button class="submit">Submit</button>`;
+ 
+$(function() {
+  $(".container").on("click", "button", function() {
+    $(".container").html(quizView);
+    
+    $('.submit').on('click', function(event) {
+      let userChoice = $('input[type="radio"]:checked').val();
+      checkAnswer(userChoice);
+      //event.(console.log(userChoice));
+    });
+  });
+});
+
+function checkAnswer() {}
+
 
 
